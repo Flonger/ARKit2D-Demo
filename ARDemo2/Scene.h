739c81d5c9356c79702a07a7cb0c24a89aa3c498
Flonger <xue@flonger.com>
2017-06-10 21:50:29 +0800

    Initial Commit

---
+++ ARDemo2/Scene.h
@@ -0,0 +1,14 @@
+//
+//  Scene.h
+//  ARDemo2
+//
+//  Created by 薛飞龙 on 2017/6/10.
+//  Copyright © 2017年 薛飞龙. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+#import <ARKit/ARKit.h>
+
+@interface Scene : SKScene
+
+@end
---
+++ ARDemo2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ARDemo2
+//
+//  Created by 薛飞龙 on 2017/6/10.
+//  Copyright © 2017年 薛飞龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+#import <ARKit/ARKit.h>
+
+@interface ViewController : UIViewController
+
+@end
